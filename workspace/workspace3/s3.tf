23230ea874fa5f9d0a9242aeee9c64b1af84608c
sanket0770 <87951759+sanket0770@users.noreply.github.com>
2023-12-07 20:58:19 +0000

    Update s3.tf

--- workspace/workspace3/s3.tf
+++ workspace/workspace3/s3.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 
 resource "aws_s3_bucket" "b" {
- bucket = "mytftestbucket0000000011111333333"
+ bucket = "mytftestbucket0000000011111111112"
 }
 
 resource "aws_s3_bucket_public_access_block" "b" {
@@ -35,15 +35,21 @@ resource "aws_s3_bucket_policy" "b" {
  "Id": "MYBUCKETPOLICY",
  "Statement": [
    {
-"Sid": "GrantAnonymousReadPermissions",
+      "Sid": "GrantAnonymousReadPermissions",
       "Effect": "Allow",
       "Principal": "*",
       "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::mytftestbucket0000000011111333333/*"
-   }
- ]
+      "Resource": "arn:aws:s3:::mytftestbucket0000000011111111112/*"
+   },
+  {
+    "Sid": "GrantAnonymousReadPermissions1",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:PutObject",
+      "Resource": "arn:aws:s3:::mytftestbucket0000000011111111112/*"
+  }
+]
 }
 POLICY
 depends_on = [ aws_s3_bucket_public_access_block.b ]
 }
-
