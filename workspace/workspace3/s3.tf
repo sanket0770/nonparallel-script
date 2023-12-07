a49b71edf4d93b43f20ab3522b16b94218411992
sanket0770 <87951759+sanket0770@users.noreply.github.com>
2023-12-07 23:07:49 +0000

    Update s3.tf

--- workspace/workspace3/s3.tf
+++ workspace/workspace3/s3.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 
 resource "aws_s3_bucket" "b" {
- bucket = "Non-parallel-research-s3-bucket"
+ bucket = "non-parallel-research-s3-bucket"
 }
 
 resource "aws_s3_bucket_public_access_block" "b" {
@@ -39,14 +39,14 @@ resource "aws_s3_bucket_policy" "b" {
       "Effect": "Allow",
       "Principal": "*",
       "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::Non-parallel-research-s3-bucket/*"
+      "Resource": "arn:aws:s3:::non-parallel-research-s3-bucket/*"
    },
   {
     "Sid": "GrantAnonymousReadPermissions1",
       "Effect": "Allow",
       "Principal": "*",
       "Action": "s3:PutObject",
-      "Resource": "arn:aws:s3:::Non-parallel-research-s3-bucket/*"
+      "Resource": "arn:aws:s3:::non-parallel-research-s3-bucket/*"
   }
 ]
 }
